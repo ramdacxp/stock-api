8221bae2246b2c77d30c76720151a451da82d21d
Michael Hoser <michael.hoser@outlook.com>
2025-03-25 18:28:39 +0100

    Stocks API

--- public/index.php
+++ public/index.php
@@ -4,19 +4,24 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
-require __DIR__ . '/../vendor/autoload.php';
+use App\Controllers\StocksController;
+
+require __DIR__ . "/../vendor/autoload.php";
 
 // configure logging
-const DISPLAY_ERROR_DETAILS = false;
+const DISPLAY_ERROR_DETAILS = true;
 const LOG_ERRORS = true;
 const LOG_ERROR_DETAILS = true;
 
 $app = AppFactory::create();
 
-$app->get('/', function (Request $request, Response $response, $args) {
+$app->get("/", function (Request $request, Response $response, $args) {
   $response->getBody()->write("Hello world!");
   return $response;
 });
+
+$app->get("/stocks", [StocksController::class, "list"]);
+$app->get("/stocks/{id}", [StocksController::class, "get"]);
 
 $app->addRoutingMiddleware();
 $errorMiddleware = $app->addErrorMiddleware(DISPLAY_ERROR_DETAILS, LOG_ERRORS, LOG_ERROR_DETAILS);
---
+++ src/Controllers/StocksController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controllers;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+class StocksController
+{
+  // public function __invoke(Request $request, Response $response, array $args): Response
+
+  // GET /stocks
+  public function list(Request $request, Response $response, array $args): Response
+  {
+    $data = ["MSFT", "APPL", "ALPH"];
+    return $this->jsonResponse($response, $data);
+  }
+
+  // GET /stocks/{id}
+  public function get(Request $request, Response $response, array $args): Response
+  {
+    $id = $args['id'] ?? -1;
+    if ($id == -1) {
+      return $this->errorResponse($response, "Parameter 'id' not given", 501);
+    } else {
+      $data = array('name' => 'Rob', 'id' => $id);
+      return $this->jsonResponse($response, $data);
+    }
+  }
+
+  private function errorResponse(Response $response, string $message, int $statusCode = 501): Response
+  {
+    $error = json_encode(array('error' => $message, 'code' => $statusCode));
+    $response->getBody()->write($error);
+
+    return $response
+      ->withHeader('Content-Type', 'application/json')
+      ->withStatus($statusCode);
+  }
+
+  private function jsonResponse(Response $response, array $data): Response
+  {
+    $response->getBody()->write(json_encode($data));
+    return $response
+      ->withHeader('Content-Type', 'application/json')
+      ->withStatus(200);
+  }
+
+}
